296d1008377dc53a6e713e1e35f795177004938b
MaNDu1543 <yashjain1543@gmail.com>
2025-07-31 19:58:29 +0530

    feat: add full Travel Ease app - services, Jenkins, Terraform

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,15 @@
+# outputs.tf - Expose useful values after deployment 
+ 
+output "ecs_cluster_name" { 
+  description = "Name of the ECS cluster" 
+  value       = aws_ecs_cluster.main.name 
+} 
+ 
+output "application_load_balancer_dns" { 
+  description = "DNS name of the ALB (your app URL)" 
+  value       = aws_lb.app.dns_name 
+} 
+ 
+output "application_url" { 
+  description = "Full URL to access your frontend" 
+} 
